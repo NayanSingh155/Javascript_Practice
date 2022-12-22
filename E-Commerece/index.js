a6305005536a97c2c9adbd473ea1654c0f69a52e
NayanSingh155 <105914443+NayanSingh155@users.noreply.github.com>
2022-12-22 14:16:02 +0530

    index done

---
+++ E-Commerece/index.js
@@ -0,0 +1,18 @@
+function Reebook() {
+    console.log("inside");
+}
+
+function Men() {
+    console.log("inside Men")
+}
+
+function Women() {
+    console.log("inside women")
+}
+
+function Login() {
+    console.log("inside Login")
+}
+function SignUp() {
+    console.log("inside Signup")
+}
