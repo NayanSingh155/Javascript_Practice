badba47269f3c40a186aa71d793e1a7007b1fb8c
NayanSingh155 <105914443+NayanSingh155@users.noreply.github.com>
2022-12-26 20:56:53 +0530

    slider

--- Counter-Game/index.js
+++ Counter-Game/index.js
@@ -1,26 +1,26 @@
 
-    let h2 = document.getElementById("result");
-    let Time_Start = 10;
-    function counter() {
-        let inp = document.getElementById("typing").value;
-        console.log(inp)
-        Time_Start = Time_Start - 1;
-        h2.innerText = Time_Start;
-        if (inp === "tera chehra hai aaine jaisa") {
-            // alert("Winner Yeah!!!")
-            h2.innerText="Winner Yeah!!!"
-        
-        }
-        if (Time_Start === 0) {
-            // alert("Ooh Looser!!")
-            clearInterval(id)
-            h2.innerText="Ooh Looser!!"
+let h2 = document.getElementById("result");
+let Time_Start = 10;
+function counter() {
+    let inp = document.getElementById("typing").value;
+    console.log(inp)
+    Time_Start = Time_Start - 1;
+    h2.innerText = Time_Start;
+    if (inp === "tera chehra hai aaine jaisa") {
+        // alert("Winner Yeah!!!")
+        h2.innerText = "Winner Yeah!!!"
 
-        }
     }
-    function refresh(){
-        window.location.href="index.html";
-        // console.log("hii")
+    if (Time_Start === 0) {
+        // alert("Ooh Looser!!")
+        clearInterval(id)
+        h2.innerText = "Ooh Looser!!"
+
     }
-    // refresh()
-    let id = setInterval(counter, 1000)
+}
+function refresh() {
+    window.location.href = "index.html";
+    // console.log("hii")
+}
+// refresh()
+let id = setInterval(counter, 1000);
