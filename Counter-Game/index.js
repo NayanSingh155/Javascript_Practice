3f22ec313f3dd38d5dfd7fc2511b1c1898cc664c
NayanSingh155 <105914443+NayanSingh155@users.noreply.github.com>
2022-12-26 17:43:37 +0530

    game

--- Counter-Game/index.js
+++ Counter-Game/index.js
@@ -6,16 +6,21 @@
         console.log(inp)
         Time_Start = Time_Start - 1;
         h2.innerText = Time_Start;
-        if (inp === "Tera chehra hai aaine jaisa") {
+        if (inp === "tera chehra hai aaine jaisa") {
             // alert("Winner Yeah!!!")
             h2.innerText="Winner Yeah!!!"
         
         }
         if (Time_Start === 0) {
             // alert("Ooh Looser!!")
-            h2.innerText="Ooh Looser!!"
             clearInterval(id)
+            h2.innerText="Ooh Looser!!"
 
         }
     }
+    function refresh(){
+        window.location.href="index.html";
+        // console.log("hii")
+    }
+    // refresh()
     let id = setInterval(counter, 1000)
