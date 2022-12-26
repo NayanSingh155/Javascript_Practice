54f3da16e1f70c52b4c3540a6cff1b610fab67fc
NayanSingh155 <105914443+NayanSingh155@users.noreply.github.com>
2022-12-26 16:01:53 +0530

    a

--- asynchronusProgramming/index.js
+++ asynchronusProgramming/index.js
@@ -1,66 +1,102 @@
-//Synchronus execution: One line at a time . Js executes line by line . A lmost every programming language executes code line by line.
+// //Synchronus execution: One line at a time . Js executes line by line . A lmost every programming language executes code line by line.
 
-// ex:
-//  console.log(1);
+// // ex:
+// //  console.log(1);
+// //  console.log(2);
+// //  console.log(3);
+// //  console.log(4);
+
+// //  so Js executeing multiple code with browser help 
+
+// // 1.setTimeout(() => {
+
+// // }, timeout);
+
+// // 2.setInterval(() => {
+
+// // }, interval);
+
+// // 3.clearInterval();
+
+
+// // -----setTimeout-----
+
+// function test(){
+//     console.log("Hey, I am here");
+// }
+// console.log(1);
 //  console.log(2);
 //  console.log(3);
 //  console.log(4);
+// //  setTimeout(test,3000);
+// //  setTimeout(test,5000);
 
-//  so Js executeing multiple code with browser help 
+//  //afetr 3 and 5 sec You will get Hey,I am there
 
-// 1.setTimeout(() => {
-    
-// }, timeout);
+// // ---------setInterval-----
 
-// 2.setInterval(() => {
-    
-// }, interval);
+// // function test(){
+// //     document.body.append("Hello, I am Here");
+// // }
+// //  console.log(1);
+// //  console.log(2);
+// //  console.log(3);
+// //  console.log(4);
 
-// 3.clearInterval();
+// //  setInterval(test,1000);
 
 
-// -----setTimeout-----
+// // It will append Hey I am here every 3 sec 
 
-function test(){
-    console.log("Hey, I am here");
-}
-console.log(1);
- console.log(2);
- console.log(3);
- console.log(4);
-//  setTimeout(test,3000);
-//  setTimeout(test,5000);
-
- //afetr 3 and 5 sec You will get Hey,I am there
-
-// ---------setInterval-----
+// // ----------clearInterval-----
 
 // function test(){
 //     document.body.append("Hello, I am Here");
 // }
-//  console.log(1);
+// console.log(1);
 //  console.log(2);
 //  console.log(3);
 //  console.log(4);
+// //  var id=setInterval(test,2000);
+// // --it execute every 2 sec
+// //  clearInterval(id);
+// // --but it clear these code after 2sec
 
-//  setInterval(test,1000);
 
-
-// It will append Hey I am here every 3 sec 
-
-// ----------clearInterval-----
 
 function test(){
-    document.body.append("Hello, I am Here");
+    console.log("Hey Hiii")
 }
-console.log(1);
- console.log(2);
- console.log(3);
- console.log(4);
-//  var id=setInterval(test,2000);
-// --it execute every 2 sec
-//  clearInterval(id);
-// --but it clear these code after 2sec
+console.log(1)
+console.log(2)
+console.log(3)
+console.log(4)
+
+setTimeout(test,3000);
+setTimeout(test,5000);
+
+// ------setInterval------
+
+function test(){
+    console.log("hiii interval jii")
+}
+console.log(1)
+console.log(2)
+console.log(3)
+console.log(4)
+
+setInterval(test,2000)
 
 
+// -------------clearTimeout-----------
 
+function test() {
+    document.body.append("hii")
+}
+
+console.log(1)
+console.log(2)
+console.log(3)
+
+var id = setInterval(test,2000)
+clearInterval(id)
