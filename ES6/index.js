62cee0253e975ec0e7f7e103cb2f8ae9af7d4f42
NayanSingh155 <105914443+NayanSingh155@users.noreply.github.com>
2022-12-29 21:33:47 +0530

    yt

---
+++ ES6/index.js
@@ -0,0 +1,40 @@
+let API=`AIzaSyDUwUBpkaP6blPrJZjMokvPwzB-z4aEW2k`;
+
+let getData=async()=>{
+    try{
+        let query=document.getElementById('query').value;
+        let res=await fetch(`https://youtube.googleapis.com/youtube/v3/search?q=${query}&key=${API}&part=snippet&maxResults=20`);
+        let {items}=await res.json();
+        let array_of_videos=items;
+        appendVideos(array_of_videos)
+        console.log(array_of_videos)
+    }
+    catch(err){
+        console.log('err :',err);
+    }
+
+}
+let search_results=document.getElementById('search_results');
+
+let appendVideos=(data)=>{
+    search_results.innerHTML=null;
+
+    data.forEach(({snippet:{title},id:{videoId}}) => {
+        
+
+        let div=document.createElement('div');
+        let iframe=document.createElement('iframe');
+        iframe.src=`https://www.youtube.com/embed/${videoId}`;
+
+        iframe.width="100%";
+        iframe.height="100%";
+         iframe.allow='fullscreen';
+        let name=document.createElement('h5');
+        name.innerText=title;
+
+        div.append(iframe,name);
+        search_results.append(div);
+
+        console.log(name,videoId);
+    });
+}
