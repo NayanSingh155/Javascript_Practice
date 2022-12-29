d87797aa769de9c72c7c05700c12e28aa198bc7a
NayanSingh155 <105914443+NayanSingh155@users.noreply.github.com>
2022-12-29 21:47:12 +0530

    yt

--- ES6/index.js
+++ ES6/index.js
@@ -29,12 +29,21 @@ let appendVideos=(data)=>{
         iframe.width="100%";
         iframe.height="100%";
          iframe.allow='fullscreen';
-        let name=document.createElement('h5');
-        name.innerText=title;
+        let Title=document.createElement('h5');
+        Title.innerText=title;
 
-        div.append(iframe,name);
+        div.append(iframe,Title);
         search_results.append(div);
+        
 
-        console.log(name,videoId);
+        // let data={
+        //     snippet:snippet,
+        //     videoId:videoId,
+        // }
+
+        div.onclick=()=>{
+            localStorage.setItem('clicked_video',JSON.stringify(data));
+        }
+        console.log(title,videoId);
     });
 }
