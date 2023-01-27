b9e5ad4030000a32846579c44a7255bd9ebfae1c
NayanSingh155 <105914443+NayanSingh155@users.noreply.github.com>
2023-01-27 21:46:37 +0530

    ok

--- Dom2/index.js
+++ Dom2/index.js
@@ -1,10 +1,19 @@
-function signUp(){
+// function signUp(){
+//     let name=document.getElementById("name").value;
+//     let email=document.getElementById("email").value;
+//     let mobile=document.getElementById("mobile").value;
+
+//     document.getElementById("dn").innerText=name;
+//     document.getElementById("de").innerText=email;
+//     document.getElementById("dm").innerText=mobile;
+
+// }
+
+function signIn(){
     let name=document.getElementById("name").value;
     let email=document.getElementById("email").value;
-    let mobile=document.getElementById("mobile").value;
-
-    document.getElementById("dn").innerText=name;
-    document.getElementById("de").innerText=email;
-    document.getElementById("dm").innerText=mobile;
-    
+    let gender=document.getElementById("gender").value;
+    document.getElementById("em").innerText=email;
+    document.getElementById("nm").innerText=name;
+    document.getElementById("gn").innerText=gender;
 }
