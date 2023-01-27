87b734dc1572c7e46da4fe5a358adcc1e3d70766
NayanSingh155 <105914443+NayanSingh155@users.noreply.github.com>
2023-01-27 14:37:42 +0530

    ok

--- Dom/index.js
+++ Dom/index.js
@@ -1,7 +1,16 @@
-function likeMe(){
-    alert("someone liked me")
-}
-function dislikeMe(){
-    // alert("someone disliked me!!")
-    console.log("disliked")
-}
+// function likeMe(){
+//     alert("someone liked me")
+// }
+// function dislikeMe(){
+//     // alert("someone disliked me!!")
+//     console.log("disliked")
+// }
+
+let x= document.getElementById("head")
+x.innerText="Now I am Changed I am new Head"
+
+let y= document.getElementById("para")
+y.innerText="Now I am N ew Para"
+
+let z= document.getElementById("div")
+z.innerText="new div"
