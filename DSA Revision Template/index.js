dc6e89ffdca9259b92d581e5d95d17216d7eddf8
NayanSingh155 <105914443+NayanSingh155@users.noreply.github.com>
2023-01-30 16:35:24 +0530

    ok

--- DSA Revision Template/index.js
+++ DSA Revision Template/index.js
@@ -1,6 +1,6 @@
 document.querySelector("form").addEventListener("submit", dsaFun);
+var queArr = JSON.parse(localStorage.getItem("dsa")) || [];
 
-let queArr = JSON.parse(localStorage.getItem("dsa")) || [];
 console.log(queArr)
 function dsaFun(event) {
     event.preventDefault();
@@ -14,5 +14,46 @@ function dsaFun(event) {
     queArr.push(dsaObj)
     // console.log(queArr)
     localStorage.setItem("dsa", JSON.stringify(queArr));
+    displayTable(queArr)
+}
 
+function displayTable(queArr) {
+    document.querySelector("tbody").innerHTML = "";
+    queArr.forEach((elem, i) => {
+        let tr = document.createElement("tr");
+        let td1 = document.createElement("td")
+        let td2 = document.createElement("td")
+        let td3 = document.createElement("td")
+        let td4 = document.createElement("td")
+        let dlt = document.createElement("button")
+        dlt.innerText = "Delete";
+        dlt.addEventListener("click", dltFun);
+        function dltFun() {
+            console.log("delete")
+            queArr.splice(i,1)
+            // console.log(queArr)
+            window.location.reload();
+            localStorage.setItem("dsa", JSON.stringify(queArr));
+        }
+
+        td1.innerText = elem.title;
+        td2.innerText = elem.link;
+        td3.innerText = elem.difficulty;
+
+        if (elem.difficulty === "Easy") {
+            td4.innerText = "No";
+        } else if (elem.difficulty === "Hard") {
+            td4.innerText = elem.innerText = "YES";
+        }
+        else if (elem.difficulty === "Medium") {
+            td4.innerText = elem.innerText = "YES";
+        }
+        else {
+            td4.innerText = elem.innerText = "";
+        }
+        tr.append(td1, td2, td3, td4, dlt)
+        console.log(tr)
+        document.querySelector("tbody").append(tr);
+    });
+    // window.location.reload()
 }
